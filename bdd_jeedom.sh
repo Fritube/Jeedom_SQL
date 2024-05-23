c034727c61d0f1ef249cda869be49f187a98a594
Fritube <50775073+Fritube@users.noreply.github.com>
2024-05-23 13:38:10 +0200

    bdd_jeedom.sh

--- bdd_jeedom.sh
+++ bdd_jeedom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "----------------------------------------------------------------"
 # Vérifier si l'utilisateur a les droits d'administration
 if [ "$(id -u)" -ne 0 ]; then
   echo "Ce script doit être exécuté avec les privilèges d'administration (utilisez sudo)." 1>&2
@@ -19,6 +19,23 @@ else
         echo "UFW a été installé avec succès."
     else
         echo "Une erreur s'est produite lors de l'installation de UFW."
+        exit 1
+    fi
+fi
+
+# Vérifier si phpmyadmin est déjà installé
+if [ -x "$(command -v phpmyadmin)" ]; then
+  echo "phpmyadmin est déjà installé sur ce système."
+else
+    # Installation de ufw
+    echo "Installation du paquet phpmyadmin... (Retenez bien le mdp pour l'utilisateur root de phpmyadmin)"
+    apt-get update
+    apt-get install -y phpmyadmin
+    # Vérifier si l'installation a réussi
+    if [ $? -eq 0 ]; then
+        echo "phpmyadmin a été installé avec succès."
+    else
+        echo "Une erreur s'est produite lors de l'installation de phpmyadmin."
         exit 1
     fi
 fi
@@ -45,18 +62,18 @@ else
   exit 1
 fi
 
-#Supprimer le mdp root de mysql/mariadb
-echo "Arrêt de MySQL..."
-systemctl stop mysql
-mysqld_safe --skip-grant-tables &
-echo "Redémarrage de MySQL..."
-systemctl start mysql
+systemctl restart mariadb
+systemctl restart mysql
+
+echo "Quel est le mot de passe de l'utilisateur root de phpmyadmin ?"
+read -s mdp_php
 
 #Modification de l'autorisation des ports
 PORT=3306
 DECISION=1
 while [ $DECISION -eq 1 ]; do
     # Demander à l'utilisateur une adresse IP
+    echo "----------------------------------------------------------------"
     echo "Bonjour ! Quelle est l'adresse IP sur laquelle vous voulez installer l'IHM ? (Celle-ci doit être fixe et dans votre LAN)"
     read ip
     
@@ -69,12 +86,13 @@ while [ $DECISION -eq 1 ]; do
     # Autoriser l'accès depuis l'adresse IP spécifiée au port spécifié
     ufw allow from "$ip" to any port "$PORT"
 
-    mysql -u root <<EOF
+    mysql -u root --password="$mdp_php" <<EOF
 GRANT ALL PRIVILEGES ON jeedom.* TO '$user'@'$ip' IDENTIFIED BY '$mdp' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
     
     # Demander si l'utilisateur veut ajouter une autre adresse IP
+    echo "----------------------------------------------------------------"
     echo "Avez-vous une autre adresse IP à ajouter ? (Y/N)"
     read dec
     
@@ -86,3 +104,4 @@ done
 cd ..
 rm -rf Jeedom_SQL
 echo "Configuration terminée"
+echo "----------------------------------------------------------------"
